da09912acef946c49987b8961f295365edae21a3
painter98 <emailmepallavi@gmail.com>
2023-05-28 12:26:52 +0530

    change5

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,20 @@ import './../styles/App.css';
 import Tooltip from './Tooltip.js';
 
 const App = () => {
-  let text1 = 'This is a tooltip';
-  let text2 = 'This is another tooltip';
+  /*let text1 = 'This is a tooltip';
+  let text2 = 'This is another tooltip';*/
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <Tooltip text1={text1} text2={text2}/>
-        
+        {/* Do not remove the main div <Tooltip text1={text1} text2={text2}/>*/}
+        <h2 className="tooltip">
+        <Tooltip text="This is a tooltip">Hover over me</Tooltip>
+      </h2>
+      <p className="tooltip">
+        <Tooltip text="This is another tooltip">
+          Hover over me to see another tooltip
+        </Tooltip>
+      </p>
     </div>
   )
 }
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import './Tooltip.css';
+import React,{useState} from 'react';
+/*import './Tooltip.css';*/
 
-function Tooltip({text1,text2}){
-    return <div className='container'>
+function Tooltip(props){
+    /*return <div className='container'>
         <h2 className="tooltip">Hover over me
             <div className='tooltiptext'>{text1}</div>
         </h2>
         <p className="tooltip">Hover over me to see another tooltip
             <div className="tooltiptext" >{text2}</div>
         </p>
+    </div>*/
+    let [showTooltip,setShowTooltip] = useState(true);
+
+  return (
+    <div
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
+      {props.children}
+      {showTooltip && <div className="tooltiptext">{props.text}</div>}
     </div>
-}
+  );
+};
 
 export default Tooltip;
