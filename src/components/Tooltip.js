6b7ebddefde051b9fb7c03f95d7986d06d00601c
painter98 <emailmepallavi@gmail.com>
2023-05-28 11:34:56 +0530

    change4

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,11 +3,11 @@ import './Tooltip.css';
 
 function Tooltip({text1,text2}){
     return <div className='container'>
-        <h2 className="tooltiptext">Hover over me
-            <div className='tooltip'>{text1}</div>
+        <h2 className="tooltip">Hover over me
+            <div className='tooltiptext'>{text1}</div>
         </h2>
-        <p className="tooltiptext">Hover over me to see another tooltip
-            <div className="tooltip" >{text2}</div>
+        <p className="tooltip">Hover over me to see another tooltip
+            <div className="tooltiptext" >{text2}</div>
         </p>
     </div>
 }
