e7d59c22deec7eea65995deb9bc967b066c289ec
painter98 <emailmepallavi@gmail.com>
2023-05-28 12:43:40 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div <Tooltip text1={text1} text2={text2}/>*/}
-      <h2 className="tooltiptext">
+      <h2 className="tooltip">
         <Tooltip text="This is a tooltip">Hover over me</Tooltip>
       </h2>
-      <p className="tooltiptext">
+      <p className="tooltip">
         <Tooltip text="This is another tooltip">
           Hover over me to see another tooltip
         </Tooltip>
